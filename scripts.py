915f153dc7514c36178e09f6839a4c192084f916
georgefs <george.li@tagtoo.org>
2014-07-26 02:07:49 +0000

    fix scripts

--- scripts.py
+++ scripts.py
@@ -1,17 +1,28 @@
 import csv
 from models import Store, Image
+import json
+from google.appengine.ext import db
+
 def upload():
     with open('ezchoice2.csv') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            s = Store()
-            s.tags = row['tags']
-            s.point = row['latlng']
-            s.reference = row['reference'][0]
+            try:
+                s = Store()
+                s.tags = json.loads(row['types'])
+                s.point = db.GeoPt(*eval(row['latlng']))
+                s.reference = row['reference'] and row['reference'][0] or ""
+                
+                for img in eval(row['images']):
+                    image = Image()
+                    image.image = img
+                    image.tags = s.tags
+                    image.put()
+                    s.imgs.append(image)
+            except Exception as e:
+                pass
+            finally:
+                s.put()
 
-            for img in row['images']:
-                img = Image()
-                img.image = img
-                img.tags = s.tags
-                img.put()
-                s.imgs.append(img)
+
+upload()
